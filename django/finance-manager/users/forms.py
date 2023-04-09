a5c3594c425a101e019f0e5a27be1ab12d623f38
Diego Di Roberto <diroberto.diego@gmail.com>
2023-04-09 19:11:47 +0200

    Added django project and custom user model

---
+++ django/finance-manager/users/forms.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.forms import UserCreationForm as DefaultUserCreationForm, UserChangeForm as DefaultUserChangeForm
+
+from users.models import User
+
+
+class UserCreationForm(DefaultUserCreationForm):
+    class Meta:
+        model = User
+        fields = ('email',)
+
+
+class UserChangeForm(DefaultUserChangeForm):
+    class Meta:
+        model = User
+        fields = ('email',)
